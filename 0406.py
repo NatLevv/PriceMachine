e34b0c457b056f067b522e2c9078910ed6540e49
Levchenko <vit031109@yandex.ru>
2024-06-04 08:56:53 +0300

    добавление файлов

---
+++ 0406.py
@@ -0,0 +1,80 @@
+import os
+import pandas as pd
+class PriceMachine:
+    def __init__(self, folder_path):
+        self.all_data = {
+            'наименование': [],
+            'цена': [],
+            'вес': [],
+            'файл': [],
+            'цена за кг': []
+        }
+        self.folder_path = folder_path
+
+    def load_prices(self):
+        for i in range(8):
+            filename = f"price_{i}.csv"
+            if os.path.exists(filename):
+                df = pd.read_csv(filename, delimiter=',')
+                an_columns = list(df.columns)
+                for column in an_columns:
+                    if column in ("название", "продукт", "товар", "наименование"):
+                        self.all_data['наименование'] += list(df[column])
+                for column in an_columns:
+                    if column in ("цена", "розница"):
+                        self.all_data['цена'] += list(df[column])
+                for column in an_columns:
+                    if column in ("фасовка", "масса", "вес"):
+                        self.all_data['вес'] += list(df[column])
+                        self.all_data['файл'] += [filename for _ in range(len(list(df[column])))]
+                        self.all_data['цена за кг'] = [round(self.all_data['цена'][i] / self.all_data['вес'][i], 2) for i in
+                                                       range(len(self.all_data['цена']))]
+
+        #print(self.all_data)
+
+        self.main_df = pd.DataFrame(self.all_data)
+        self.main_df = self.main_df.sort_values(by=['цена за кг'])
+        #print(self.main_df)
+        return self.main_df
+        #print(main_df)
+
+
+    def export_to_html(self, filename='output.html'):
+        html_table = self.main_df.to_html(index=False)
+        with open(filename, 'w') as f:
+            f.write(html_table)
+        print(f"Данные успешно сохранены в файл: {filename}")
+
+    def export_search_results_to_html(self, found_items, filename='search_output.html'):
+        if found_items.empty:
+            print("Товар не найден.")
+        else:
+            html_table = found_items.to_html(index=False)
+            with open(filename, 'w', encoding='utf-8') as f:
+                f.write(html_table)
+            print(f"Результаты поиска успешно сохранены в файл: {filename}")
+
+    def find_text(self, search_term):
+        search_term = search_term.lower()  # Преобразование поискового запроса в нижний регистр
+        found_rows = []
+        for index, row in self.main_df.iterrows():
+            if search_term in row['наименование'].lower().split(' '):  # Сравнение в нижнем регистре
+                found_rows.append(row)
+        found_df = pd.DataFrame(found_rows)
+        if not found_df.empty:
+            print("Найденные товары:")
+            print(found_df)
+        self.export_search_results_to_html(found_df)
+
+
+if __name__ == "__main__":
+    price_machine = PriceMachine('.')
+    price_machine.load_prices()
+
+    while True:
+        search_term = input("Введите фрагмент названия товара для поиска (для выхода введите 'exit'): ")
+        if search_term.lower() == "exit":
+            print("Программа завершена.")
+            break
+        else:
+            price_machine.find_text(search_term)
